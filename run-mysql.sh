f8f3d60d15892d35f9a4fc385d7aed58e788b985
Michael J. Simons <michael@simons.ac>
2018-06-16 18:44:38 +0200

    Fix commands

--- run-mysql.sh
+++ run-mysql.sh
@@ -5,6 +5,10 @@ docker run -d -p 3306:3306 --name dailyfratze_prod \
     mysql:5.6.40 \
     --character-set-server=utf8mb4 --collation-server=utf8mb4_bin
 
-mysql --host=127.0.0.1 --port=3306  -uroot -proot --default-character-set=utf8 
+mysql --host=127.0.0.1 --port=3306  -uroot -proot --default-character-set=utf8mb4 
+
+create database dailyfratze_prod;
+
+mysql --host=127.0.0.1 --port=3306  -uroot -proot --default-character-set=utf8mb4 dailyfratze_prod  < 20180616.sql
 
 pgloader create-and-fill-import-schema.command
